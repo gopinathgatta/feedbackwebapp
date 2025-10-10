434da31bccf1a0b660924f93eb3303c652ca77f4
gopinathgatta <150040498+gopinathgatta@users.noreply.github.com>
2025-10-10 15:53:12 +0530

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,4 @@
-const API_BASE_URL = '/api';
-
+const API_BASE_URL = import.meta.env.VITE_API_BASE_UR;
 // Get auth token from localStorage
 const getAuthToken = () => {
   return localStorage.getItem('authToken');
